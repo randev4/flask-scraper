5ac361886e9e0984242b08cbf287f51de7f5f6a6
randev4 <randev4@gmail.com>
2024-03-30 15:38:52 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .app import app
-from .reddit_scraper import scrape_reddit_thread_route
-from .article_scraper import scrape_articles_route
-
-app.register_blueprint(scrape_reddit_thread_route)
-app.register_blueprint(scrape_articles_route)
