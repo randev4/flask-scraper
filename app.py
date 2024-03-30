d616fb90ef5b2cfdca843818ece788a95df5bce9
randev4 <randev4@gmail.com>
2024-03-30 15:50:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-from flask import Flask, jsonify
 
-from .reddit_scraper import scrape_reddit_thread_route
-from .article_scraper import scrape_articles_route
-
-app = Flask(__name__)
-
-app.register_blueprint(scrape_reddit_thread_route)
-app.register_blueprint(scrape_articles_route)
-
-@app.route('/')
-def home():
-    return jsonify({'message': 'Welcome to the scraping application!'})
-
-if __name__ == '__main__':
-    app.run(debug=True)
